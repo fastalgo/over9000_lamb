4e6ee0510a9ce1a95b7e624ac5e9d613e0477ae6
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-09-06 18:14:46 +0000

    u

---
+++ run0.sh
@@ -0,0 +1,1 @@
+python train.py --run 20 --woof 0 --size 128 --bs 64 --mixup 0 --sa 0 --epoch 5 --lr 0.03 --gpu 0 --opt lamb --alpha 0.999 --sched_type flat_and_anneal --ann_start 0.5 --weight_decay=0.01
